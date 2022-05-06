372b297453fa904e53203c19b019d78b7e545d16
Kamil <k-gryciuk@wp.pl>
2022-05-06 16:13:23 +0200

    Added global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './app/store';
+import GlobalStyle from './styles/globalStyle';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
