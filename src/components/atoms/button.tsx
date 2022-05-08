1f15b15534374d71d775b6f84ea6b4e783377f88
Kamil <k-gryciuk@wp.pl>
2022-05-08 13:53:38 +0200

    Added and styled button

---
+++ src/components/atoms/button.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Button = styled.input`
+  background-color: ${({ theme }) => theme.colors.second};
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: 900;
+  border: 2px solid ${({ theme }) => theme.colors.second};
+  border-radius: 30px;
+  padding: 10px 40px;
+  cursor: pointer;
+  font-size: 15px;
+  letter-spacing: 1px;
+  &:hover {
+    color: ${({ theme }) => theme.colors.second};
+    background-color: ${({ theme }) => theme.colors.white};
+  }
+
+`;
+
+export default Button;
