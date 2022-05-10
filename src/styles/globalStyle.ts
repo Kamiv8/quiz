f990477d52beaa7d86f1a5912674860e34f11cb5
Kamil <k-gryciuk@wp.pl>
2022-05-10 11:38:55 +0200

    Styled scrollbar

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import theme from './theme';
 
 export default createGlobalStyle`
   *,::before,*::after {
@@ -6,5 +7,18 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: 'Inter', sans-serif;
+  }
+  
+  body {
+    &::-webkit-scrollbar {
+      width: 10px;
+    }
+    &::-webkit-scrollbar-thumb {
+      background: ${theme.colors?.second};
+      border-radius: 10px;
+      &:hover {
+        background: ${theme.colors?.blue};
+      }
+    }
   }
 `;
